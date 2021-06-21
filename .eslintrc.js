3ace8b1f980412a556a83256a7b66b59e1259cb9
Juliette Baron <juliette.baron.33@gmail.com>
2021-06-21 11:26:53 +0200

    eslint ok

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,9 @@
 module.exports = {
   root: true,
   env: {
-<<<<<<< HEAD
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
-=======
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
     parser: "babel-eslint",
   },
@@ -28,4 +12,3 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
->>>>>>> fa312fbdef439cb8c1d4ad82e0531a9cfbb7978e
