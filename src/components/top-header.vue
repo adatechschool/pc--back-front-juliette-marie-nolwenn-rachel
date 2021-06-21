fa312fbdef439cb8c1d4ad82e0531a9cfbb7978e
Rachel <rachel.bouhier@outlook.com>
2021-06-21 11:11:16 +0200

    [Refacto] Modif produits

--- src/components/top-header.vue
+++ src/components/top-header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="lg" type="light" variant="info">
       <b-navbar-brand href="#"
         >Anciens meubles pour une nouvelle vie</b-navbar-brand
       >
@@ -11,7 +11,7 @@
         <b-navbar-nav>
           <b-nav-item to="/">Home</b-nav-item>
           <b-nav-item to="/about">A propos</b-nav-item>
-          <b-nav-item to="/connexion">Connexion</b-nav-item>
+          <b-nav-item to="/connexion">Se connecter</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
@@ -27,22 +27,6 @@
               >Chercher</b-button
             >
           </b-nav-form>
-
-          <!-- <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>-->
-
-          <!-- <b-nav-item-dropdown right> -->
-          <!-- Using 'button-content' slot -->
-          <!-- <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>-->
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
